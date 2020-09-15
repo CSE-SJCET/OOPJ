de0e31e77f3fbdf087123e3f008b565b0982ec30
Sarju S <sarju.s@sjcetpalai.ac.in>
2020-09-15 05:34:32 +0530

    First Commit

---
+++ S3 2019 Batch/src/basicPrograms/HelloJava.java
@@ -0,0 +1,16 @@
+/**
+* The HelloJava program implements an application that
+* simply displays "Hello Java!" to the standard output.
+*
+* @author  Sarju S
+* @version 1.0
+* @since   2020-09-15 
+*/
+package basicPrograms;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		// Prints Hello, World! on standard output.
+		System.out.println("Hello World!");
+	}
+}
